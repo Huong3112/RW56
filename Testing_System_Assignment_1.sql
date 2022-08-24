d80cefa779e903390a26bfccb93a14220a52f5c0
Huong3112 <111965894+Huong3112@users.noreply.github.com>
2022-08-25 00:46:45 +0900

    Create Testing_System_Assignment_1.sql

---
+++ Testing_System_Assignment_1.sql
@@ -0,0 +1,111 @@
+DROP DATABASE IF EXISTS Testing_System_Assignment_1;
+CREATE DATABASE Testing_System_Assignment_1 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE Testing_System_Assignment_1;
+
+DROP TABLE IF EXISTS `Department`;
+CREATE TABLE `Department`(
+	DepartmentID	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	DepartmentName	VARCHAR(30) NOT NULL UNIQUE KEY
+);
+
+DROP TABLE IF EXISTS `Position`;
+CREATE TABLE `Position`(
+	PositionID	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    PositionName	ENUM('Dev', 'Test', 'Scrum Master', 'PM') NOT NULL UNIQUE KEY
+    );
+
+DROP TABLE IF EXISTS `Account`;
+CREATE TABLE `Account`(
+	AccountID	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Email	VARCHAR(30) NOT NULL UNIQUE KEY,
+    Username	VARCHAR(30) NOT NULL UNIQUE KEY,
+    Fullname	VARCHAR(30) NOT NULL,
+    DepartmentID	TINYINT UNSIGNED NOT NULL,
+    PositionID	TINYINT UNSIGNED NOT NULL,
+	CreateDate	DATETIME DEFAULT NOW(),
+    FOREIGN KEY(DepartmentID) REFERENCES `Department`(DepartmentID),
+    FOREIGN KEY(PositionID) REFERENCES `Position`(PositionID)
+    );
+    
+DROP TABLE IF EXISTS `Group`;
+CREATE TABLE `Group`(
+	GroupID	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    GroupName	VARCHAR(30) NOT NULL UNIQUE KEY,
+    CreatorID	TINYINT UNSIGNED,
+    CreateDate	DATETIME DEFAULT NOW(),
+    FOREIGN KEY(CreatorID) REFERENCES `Account`(AccountID)
+    );
+    
+DROP TABLE IF EXISTS `GroupAccount`;
+CREATE TABLE `GroupAccount`(
+	GroupID	TINYINT UNSIGNED NOT NULL,
+    AccountID	TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY(GroupID, AccountID),
+    JoinDate	DATETIME DEFAULT NOW(),
+    FOREIGN KEY(GroupID) REFERENCES `Group`(GroupID),
+    FOREIGN KEY(AccountID) REFERENCES `Account`(AccountID)
+	);
+    
+    
+DROP TABLE IF EXISTS `TypeQuestion`;
+CREATE TABLE `TypeQuestion`(
+	TypeID	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    TypeName	ENUM('Essay', 'Multiple-choice')
+    );
+    
+DROP TABLE IF EXISTS `CategoryQuestion`;
+CREATE TABLE `CategoryQuestion`(
+	CategoryID	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    CategoryName VARCHAR(50) NOT NULL UNIQUE KEY
+    );
+    
+DROP TABLE IF EXISTS `Question`;
+CREATE TABLE `Question`(
+	QuestionID	TINYINT UNSIGNED AUTO_INCREMENT,
+    Content	VARCHAR(100) NOT NULL,
+    CategoryID	TINYINT UNSIGNED NOT NULL,
+    TypeID	TINYINT UNSIGNED NOT NULL,
+    CreatorID	TINYINT UNSIGNED NOT NULL,
+    CreateDate	DATETIME DEFAULT NOW(),
+    PRIMARY KEY(QuestionID, CategoryID, TypeID, CreatorID),
+    FOREIGN KEY(CategoryID) REFERENCES `CategoryQuestion`(CategoryID),
+    FOREIGN KEY(TypeID) REFERENCES `TypeQuestion`(TypeID),
+    FOREIGN KEY(CreatorID) REFERENCES `Group`(CreatorID)
+    );
+
+DROP TABLE IF EXISTS `Answer`;
+CREATE TABLE `Answer`(
+	AnswerID	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    Content	VARCHAR(100) NOT NULL,
+    QuestionID	TINYINT UNSIGNED NOT NULL,
+    isCorrect	BIT DEFAULT 1,
+    FOREIGN KEY(QuestionID) REFERENCES `Question`(QuestionID)
+    );
+    
+DROP TABLE IF EXISTS `Exam`;
+CREATE TABLE `Exam`(
+	ExamID	TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `Code`	VARCHAR(10) NOT NULL,
+    Title	VARCHAR(50) NOT NULL,
+    CategoryId	TINYINT UNSIGNED NOT NULL,
+    Duration	TINYINT UNSIGNED NOT NULL,
+    CreatorID	TINYINT UNSIGNED NOT NULL,
+    CreateDate	DATETIME DEFAULT NOW(),
+    FOREIGN KEY(CategoryID) REFERENCES `CategoryQuestion`(CategoryID),
+    FOREIGN KEY(CreatorID) REFERENCES `Account`(AccountID)
+    );
+    
+DROP TABLE IF EXISTS `ExamQuestion`;
+CREATE TABLE `ExamQuestion`(
+	ExamID	TINYINT UNSIGNED NOT NULL,
+    QuestionID	TINYINT UNSIGNED NOT NULL,
+    PRIMARY KEY(ExamID, QuestionID),
+    FOREIGN KEY(ExamID) REFERENCES `Exam`(ExamID),
+    FOREIGN KEY(QuestionID) REFERENCES `Question`(QuestionID)
+    );
+
+
+
+
+        
